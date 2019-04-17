cb490a7009f3902695a3456292ad36a8b00713a5
Madhan Mohan <ncmadhan@Madhans-MacBook-Pro-3.local>
2019-04-17 06:53:46 +0530

    Config Changes

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -1,7 +1,11 @@
 import boto3
 import json
 
-ec2 = boto3.client('ec2')
 def lambda_handler(event, context):
-    response = ec2.describe_availability_zones()
+    response = {
+	  "name": "John",
+	  "age": 30,
+	  "city": "New York"
+	}
+
     return {"statusCode": 200, "body": json.dumps(response)}
